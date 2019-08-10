ea1f774fc3a3d482ea0e41783849bb9ff6d50560
Steven Hugg <hugg@fasterlight.com>
2019-08-09 23:03:44 -0400

    added data.c/h

---
+++ data.c
@@ -0,0 +1,16 @@
+
+//include nametables for all the screens such as title or game over
+
+#include "title_nam.h"
+#include "level_nam.h"
+#include "gameover_nam.h"
+#include "welldone_nam.h"
+
+//include nametables for levels
+
+#include "level1_nam.h"
+#include "level2_nam.h"
+#include "level3_nam.h"
+#include "level4_nam.h"
+#include "level5_nam.h"
+
